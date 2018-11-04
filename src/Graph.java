bc371756530d843655502635ce9b76c9ae56d29b
Chaoping Guo <chaoping.guo@hotmail.com>
2018-11-04 13:26:11 -0800

    from TS to Graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,14 +1,35 @@
 import java.util.List;
 
-import org.apache.commons.math3.linear.RealMatrix;
-
 public class Graph {
-	List<Node> V;
-	List<Edge> E;
+	int[][] adjacentMatrix;
+	int numNodes;
 	
 	// Constructor I: Create a Graph with a matrix
-	Graph(RealMatrix M){
-		//TODO
+	Graph(double[][] rawInput, int start, int end){
+		numNodes = rawInput.length;
+		adjacentMatrix = new int[numNodes][numNodes];
+		
+		// threshold of correlation significance
+		double alpha = 0.01;
+		
+		// fill in the matrix
+		
+		for(int i = 0; i< numNodes; i++) {
+			for(int j = i+1; j < numNodes; j++) {
+				adjacentMatrix[i][j] = isLinked(rawInput, start, end, i, j);
+			}
+		}
+		
+		// fill the diagonals with 1s
+		for (int i = 0; i< numNodes; i++) adjacentMatrix[i][i] = 1;
+		
+		// fill the lower half using the same values
+		
+		for (int i =0; i < numNodes ; i++) {
+			for(int j= 0; j<i; j++) {
+				adjacentMatrix[i][j] = adjacentMatrix[j][i];
+			}
+		}
 	}
 	
 	static Graph symDiff(Graph G, Graph H) {
