f883f85d0f1dea42dc1a20b8057d7ea83b3c4d25
Brandon Graham-Knight <brandongk@gmail.com>
2018-11-21 13:12:58 -0800

    Test cases for symdiffedgecount and unionedgecount are in place.

--- src/test/java/ubco/cosc520/graph/TwoGraphSymDiffEdgeCountTest.java
+++ src/test/java/ubco/cosc520/graph/TwoGraphSymDiffEdgeCountTest.java
@@ -4,7 +4,7 @@ import org.apache.commons.math3.linear.MatrixUtils;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class TwoGraphSymDiffEdgeCountTest {
 
@@ -21,12 +21,31 @@ public class TwoGraphSymDiffEdgeCountTest {
                 {1, 1, 1}
         };
 
+        testMatrices(m1, m2, 0);
+    }
+
+    @Test
+    public void testOppositeMatrixReturnsZero() {
+        double[][] m1 = {
+                {1, 0, 1},
+                {0, 1, 1},
+                {1, 1, 1}
+        };
+        double[][] m2 = {
+                {1, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1}
+        };
+        testMatrices(m1, m2, 3);
+    }
+
+    private void testMatrices(double[][] m1, double[][] m2, int result) {
         Graph g1 = new UndirectedAdjacencyMatrixGraph(MatrixUtils.createRealMatrix(m1));
         Graph g2 = new UndirectedAdjacencyMatrixGraph(MatrixUtils.createRealMatrix(m2));
 
         TwoGraphOperator<Integer> twoGraphSymDiff = new TwoGraphSymDiffEdgeCount();
         Integer edgeNumOfSymDiff = twoGraphSymDiff.operate(g1, g2);
 
-        assertThat(edgeNumOfSymDiff, is(0));
+        assertThat(edgeNumOfSymDiff, is(result));
     }
 }
