b8448f3c135d59423b2ae35a59e2cd53c51ca866
Chaoping Guo <chaoping.guo@hotmail.com>
2018-11-30 16:55:12 -0800

    processed data

---
+++ src/test/resources/plotTS.R
@@ -0,0 +1,17 @@
+TS = t(read.csv("aq_short.csv", header = F))
+rownames(TS) = NULL
+Seg = t(read.csv("aq_short_seg.csv", header = F))
+require(reshape2)
+require(ggplot2)
+meltdf = melt(TS)
+names(meltdf) = c("Time","Variable","Value")
+meltdf$Variable = as.factor(meltdf$Variable)
+
+ts_plot = ggplot(meltdf,aes(x=Time,y=Value,colour=Variable,group=Variable)) + geom_line(size = 1)
+
+for(i in Seg){
+  ts_plot = ts_plot + geom_vline(xintercept = i, 
+                                      color = "blue", size=0.5)
+}
+
+ts_plot
